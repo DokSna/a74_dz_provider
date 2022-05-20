57432840442c5adf39e52e5a0304d81015a336be
Artem <31787423+DokSna@users.noreply.github.com>
2022-05-20 22:02:34 +0300

    Готово

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,75 @@
-// import 'dart:math';
-
+import 'dart:math';
 import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
+
+class RandomColor extends ChangeNotifier {
+  bool _switchStatus;
+  Color _titleColor;
+  Color _figureColor;
+
+  RandomColor(this._switchStatus, this._titleColor, this._figureColor);
+
+  void getRandomColor() {
+    _titleColor = Color.fromRGBO(
+        Random().nextInt(255), Random().nextInt(255), Random().nextInt(255), 1);
+    _figureColor = Color.fromRGBO(
+        Random().nextInt(255), Random().nextInt(255), Random().nextInt(255), 1);
+    _switchStatus = !_switchStatus;
+    notifyListeners();
+  }
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  RandomColor newRandomColor = RandomColor(
+      true,
+      const Color.fromRGBO(255, 215, 0, 1),
+      const Color.fromRGBO(30, 144, 255, 1));
+
+  MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Random colors',
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          title: const Text(
-            'Homework Provider',
-            style: TextStyle(color: Colors.green),
-          ),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              AnimatedContainer(
-                duration: const Duration(seconds: 1),
-                color: Colors.blue,
-                width: 200,
-                height: 200,
-              ),
-              const Switcher(),
-            ],
-          ),
-        ),
+    return ChangeNotifierProvider<RandomColor>(
+      create: ((context) => newRandomColor),
+      child: const MaterialApp(
+        home: HomePage(),
       ),
     );
   }
 }
 
-class Switcher extends StatefulWidget {
-  const Switcher({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-  @override
-  State<Switcher> createState() => _SwitcherState();
-}
-
-class _SwitcherState extends State<Switcher> {
-  bool _giveVerse = true;
   @override
   Widget build(BuildContext context) {
-    return Switch(
-      value: _giveVerse,
-      onChanged: (value) {
-        setState(() {
-          _giveVerse = value;
-        });
-      },
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          'Homework Provider',
+          style: TextStyle(color: context.watch<RandomColor>()._titleColor),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedContainer(
+              duration: const Duration(seconds: 1),
+              color: context.watch<RandomColor>()._figureColor,
+              width: 200,
+              height: 200,
+            ),
+            Switch(
+                value: context.watch<RandomColor>()._switchStatus,
+                onChanged: (_) => context.read<RandomColor>().getRandomColor()),
+          ],
+        ),
+      ),
     );
   }
 }
